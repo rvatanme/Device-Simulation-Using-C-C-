fcea193af7098f0af1f5b036b5481c9a8ab0db5b
rvatanme <rvatanme@asu.edu>
2021-11-28 23:24:26 -0700

    Update 1d-pn.py

--- Python/1d-pn.py
+++ Python/1d-pn.py
@@ -75,19 +75,136 @@ dx = dx/20
 n_max = math.floor(x_max/dx) +1
 dx = dx/Ldi                # Renormalize lengths with Ldi
 
-# Setup the normalized doping for a pn diode with eqaul N-side and P-side length size
-dop = [0.0]*n_max
+# Setup the normalized doping by ni for a pn diode with eqaul N-side and P-side length size        
+dop = [0]*n_max
 for i in range(n_max):
     if i <= n_max/2:
         dop[i] = -Na/ni
     else:
-        dop[i] = Nd/ni
+        dop[i] = Nd/ni 
+        
+ # Initialize the potential based on the requirement of charge neutrality throughout the whole structure
+fi = [0]*n_max
+n = [0]*n_max
+p = [0]*n_max
+for i in range(n_max):
+    zz = 0.5*dop[i]
+    if zz > 0:
+        xx = zz*(1 + math.sqrt(1+1/(zz*zz)))
+    if zz <  0:
+        xx = zz*(1 - math.sqrt(1+1/(zz*zz)))
+    fi[i] = math.log(xx)
+    n[i] = xx
+    p[i] = 1/xx
 
-               
+delta_acc = 1E-5               # Set the Tolerance
+
+##########################################################################
+##                                                                      ##
+##               EQUILIBRIUM  SOLUTION PART BEGINS                      ##
+##                                                                      ##
+##########################################################################
+
+#(A) Define the elements of the coefficient matrix for the internal nodes and
+#    initialize the forcing function
+a = [0]*n_max
+c = [0]*n_max
+b = [0]*n_max
+f = [0]*n_max
+dx2 = dx*dx
+for i in range(n_max):
+    a[i] = 1/dx2
+    c[i] = 1/dx2
+    b[i] = -(2/dx2+math.exp(fi[i])+math.exp(-fi[i]))
+    f[i] = math.exp(fi[i]) - math.exp(-fi[i]) - dop[i] - fi[i]*(math.exp(fi[i])+math.exp(-fi[i]))
+
+#(B) Define the elements of the coefficient matrix and initialize the forcing function at the ohmic contacts
+a[0] = 0
+c[0] = 0
+b[0] = 1
+f[0] = fi[0]
+a[n_max-1] = 0
+c[n_max-1] = 0
+b[n_max-1] = 1
+f[n_max-1] = fi[n_max-1]
+
+#  Start the iterative procedure for the solution of the linearized Poisson equation using LU decomposition method:
+flag_conv = 0           # convergence of the Poisson loop
+k_iter= 0
+alpha = [0]*n_max
+beta = [0]*n_max
+v = [0]*n_max
+delta = [0]*n_max
+while flag_conv == 0:          
+    k_iter = k_iter + 1
+    alpha[0] = b[0]
+    for i in range(1, n_max):
+        beta[i]=a[i]/alpha[i-1]
+        alpha[i]=b[i]-beta[i]*c[i-1]
+    
+    # Solution of Lv = f %    
+    v[0] = f[0]
+    for i in range(1,n_max):
+        v[i] = f[i] - beta[i]*v[i-1]
+    
+    # Solution of U*fi = v %
+    temp = v[n_max - 1]/alpha[n_max - 1]
+    delta[n_max - 1] = temp - fi[n_max - 1]
+    fi[n_max - 1]=temp
+    for i in range(n_max-1):       #delta
+        i = n_max - 2 - i
+        temp = (v[i]-c[i]*fi[i+1])/alpha[i]
+        delta[i] = temp - fi[i]
+        fi[i] = temp
+    
+    delta_max = 0
+    
+    for i in range(n_max):
+        xx = abs(delta[i])
+        if xx > delta_max:
+            delta_max=xx
+            
+    print (delta_max)
+    if(delta_max < delta_acc):
+        flag_conv = 1
+    else:
+        for i in range(1,n_max-1):
+            b[i] = -(2/dx2 + math.exp(fi[i]) + math.exp(-fi[i]));
+            f[i] = math.exp(fi[i]) - math.exp(-fi[i]) - dop[i] - fi[i]*(math.exp(fi[i]) + math.exp(-fi[i]))
+            
+# Printing out the results
+xx1 = [0]*n_max
+Ec = [0]*n_max
+ro = [0]*n_max
+el_field1 = [0]*n_max
+el_field2 = [0]*n_max
+xx1[i] = dx*1e4
+for i in range(1,n_max-1): 
+    Ec[i] = dEc - Vt*fi[i];     #Values from the second Node
+    ro[i] = -ni*(math.exp(fi[i]) - math.exp(-fi[i]) - dop[i])
+    el_field1[i] = -(fi[i+1] - fi[i])*Vt/(dx*Ldi)
+    el_field2[i] = -(fi[i+1] - fi[i-1])*Vt/(2*dx*Ldi)
+    n[i] = math.exp(fi[i])
+    p[i] = math.exp(-fi[i])
+    xx1[i] = xx1[i-1] + dx*Ldi*1e4
 
 
 
+Ec[0] = Ec[1]
+Ec[n_max-1] = Ec[n_max-2]
+xx1[n_max-1] = xx1[n_max-2] + dx*Ldi*1e4
+el_field1[0] = el_field1[1]
+el_field2[0] = el_field2[1]
+el_field1[n_max-1] = el_field1[n_max-2]
+el_field2[n_max-1] = el_field2[n_max-2]
+nf = [x*ni for x in n]
+pf = [x*ni for x in p]
+ro[1] = ro[2]
+ro[n_max-1] = ro[n_max-2]
 
-               
 
-
+f1 = open('els_plot_py.txt', 'w')
+f1.write("x [um]\tPotential [V]\tElectric Field1 [V/cm]\tElectric Field2 [V/cm]\tElectron Densities [1/cm^3]\tHole Densities [1/cm^3]\tTotal Charge Density [C/cm^3]\tConduction Band Energy (eV)\n")
+for i in range(n_max):
+    f1.write(str(xx1[i])+'\t'+str(Vt*fi[i])+'\t'+str(el_field1[i])+'\t'+str(el_field2[i])+'\t'+str(nf[i])+'\t'+str(pf[i])+'\t'+str(ro[i])+'\t'+str(Ec[i])+'\n')
+f1.close()
